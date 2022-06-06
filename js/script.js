4ac8e034886bafcb87b8626cda79db86d11f9f30
nikitor141 <nikitor141@gmail.com>
2022-06-06 21:02:12 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,91 @@
-$(function(){
-  $('.burger').click(function(){
-    $('body').toggleClass('lock');
-    $('.burger, .header__list').toggleClass('active');
+$(function () {
+  //slider ------------------------------------------------
+  $('.portfolio__row').slick({
+    draggable: false,
+    infinite: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          infinite: true
+        }
+      }
+    ]
   });
+  //burger ------------------------------------------------
+  $('.burger').click(function () {
+    $('body,.burger, .header__list').toggleClass('active');
+  });
+  $('.list__link').click(function () {
+    $('body,.burger,.header__list').removeClass('active')
+  });
+  //анимация скрола при нажатии на якоря -----------------
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+  //popup form---------------------------------------------
+  $('.popup__input').blur(function () {
+    if (!$(this).val()) {
+      $(this).addClass('error');
+    }
+    else {
+      $(this).removeClass('error');
+    }
+  });
+  $(".popup__input_tel").mask("+7(999) 999-99-99");
+
+  $('.header__mobile-call-order,.call-order,.btn').click(function () {
+    $('body,.popup,.popup__body').addClass('active');
+  });
+  $('.popup').on('mousedown', function (event) {
+    if (event.target === this) {
+      $('body,.popup,.popup__body').removeClass('active');
+      $('.popup__input').val('').removeClass('error');
+    }
+  });
+
+
+  // test 
+  $('.tel-mail').click(function (event) {
+    event.preventDefault();
+    const TEXT_TO_COPY = $(this).data('tel-mail');
+    const COPY_TO_CLIPBOARD = (text) =>
+      navigator.clipboard.writeText(text);
+    COPY_TO_CLIPBOARD(TEXT_TO_COPY);
+    $('.tel-mail span').text('Скопированно!')
+    $('.tel-mail').mouseleave(function () {
+      $('.tel-mail span').text('Скопировать')
+
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
